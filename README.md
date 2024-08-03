1ae9043cea8d65e9d2f2056f593f1bbceb234692
Paloma <paloma.ditex@gmail.com>
2024-08-03 16:06:53 -0400

    Final PruebaEncuentrsTuSuperHero

---
+++ README.md
@@ -0,0 +1,51 @@
+## Proyecto Super Hero
+#### [Visualiza el landing completo aqui 🚀](https://singularpigeon.github.io/SuperheroAplicacion/)
+![](/assets/img/previewSuperhero.png)
+***
+### Descripción
+Desarrollo de una aplicación dinámica que permita a los usuarios buscar un Super Héroe o Super Heroína y visualizar información referente al personaje buscado.
+Proyecto de final del módulo 4 del Bootcamp Desarrollo de aplicaciones front-end trainee v2.0 - Fundamentos de desarrollo front-end, Desafío Latam.
+***
+### Estructura del proyecto
+```
+└── 📁PRUEBAEncuentraTuSuperHero
+    └── .DS_Store
+    └── .gitignore
+    └── 📁.vscode
+        └── settings.json
+    └── Apoyo Prueba - Encuentra tu SuperHero.pdf
+    └── README.md
+    └── 📁assets
+        └── 📁css
+            └── style.css
+        └── 📁img
+            └── favicon.ico
+            └── pexels-introspectivedsgn-7524996.jpg
+            └── pexels-kpaukshtite-701771.jpg
+            └── sh1.jpg
+            └── sh2.jpg
+            └── xxface.png
+        └── 📁js
+            └── JQuery.buscaHero.js
+            └── script.js
+    └── index.html
+```
+***
+### Tecnologías utilizadas
+
+![](https://img.shields.io/badge/HTML5-E34F26?style=for-the-badge&logo=html5&logoColor=white) ![](https://img.shields.io/badge/Bootstrap-563D7C?style=for-the-badge&logo=bootstrap&logoColor=white) ![](https://img.shields.io/badge/CSS3-1572B6?style=for-the-badge&logo=css3&logoColor=white) ![](https://img.shields.io/badge/jQuery-0769AD?style=for-the-badge&logo=jquery&logoColor=white) ![](https://img.shields.io/badge/JavaScript-323330?style=for-the-badge&logo=javascript&logoColor=F7DF1E)
+
+
+***
+### Requisitos
+- **Bootstrap version 5.3.3**
+- **JQuery 3.7.1**
+- **CanvasJS**
+- **Google Fonts**
+- **Font Awesome**
+***
+
+
+### Autores
+- **Desarolladora:[Paloma Rivera](https://github.com/**SingularPigeon)**
+- **Diseño: [Bootcamp Desafío Latam](desafiolatam.com)**
--- assets/js/JQuery.buscaHero.js
+++ assets/js/JQuery.buscaHero.js
@@ -1,8 +1,9 @@
+// Plugin
 jQuery.fn.buscaHero = function(token, idNumber) {
   let element = $(this);
 
-  let accessToken = token;
-  let idHero = idNumber;
+  let accessToken = token; //variable para introducir el token personal que entrega la API
+  let idHero = idNumber; // Id de número asignada a cada super hero
 
   const settings = {
     "async": true,
@@ -20,25 +21,26 @@ jQuery.fn.buscaHero = function(token, idNumber) {
     heroCard(element, response);
     chartCard(response);
   })
-  .fail(function() {
+  .fail(function() { // alerta sin hay fallas de comunicación con la API
     alert('Ooops! Parece que tenemos problemas. Intentalo más tarde.');
   });
 
   return this;
 
   // Define función para mostrar la card de super hero.
+  // Si el dato es "null" en la propiedad raza, no se muestra.
   function heroCard(element, response) {
     let heroCard = `
-      <div class="card my-3 border-danger-subtle">
+      <div class="card my-3 border-danger-subtle bg-dark h-100">
         <div class="row g-0">
           <div class="col-md-6">
             <img src="${response.image.url}" class="img rounded-start h-100 w-100 object-fit-cover" alt="...">
           </div>
-          <div class="col-md-6 text-bg-dark">
+          <div class="col-md-6 text-bg-dark rounded">
           <div class="card-header p-4 border-bottom-1 border-danger-subtle">
             <h2 class="card-title">${response.name}</h2>
-            <h3 class="card-title">${response.biography["full-name"]}</h3>
-            <p class="card-text fs-4">${response.biography["place-of-birth"]}</p>
+            <p class="card-text fs-5">Nombre completo: ${response.biography["full-name"]}</p>
+            <p class="card-text fs-5">Origen: ${response.biography["place-of-birth"]}</p>
             <p class="card-text fs-5"> Aliases: ${response.biography["aliases"].join(', ')}</p>
             <p class="card-text fs-5"> Conexiones: ${response.connections.relatives}</p>
           </div>
@@ -46,13 +48,14 @@ jQuery.fn.buscaHero = function(token, idNumber) {
               <p class="card-text fs-5"> Ocupación: ${response.work.occupation}</p>
               <p class="card-text fs-5"> Primera Aparición: ${response.biography["first-appearance"]}</p>
               <p class="card-text fs-5"> Género: ${response.appearance.gender}</p>
-              <p class="card-text fs-5"> Raza: ${response.appearance.race}</p>
+              ${response.appearance.race !== "null" ? `<p class="card-text fs-5"> Raza: ${response.appearance.race}</p>` : ''}
               <span class="me-3 card-text fs-5">Altura: ${response.appearance["height"].join(' | ')}</span>
               <span class="card-text fs-5">Peso: ${response.appearance["weight"].join(' | ')}</span>
               <p class="card-text fs-5">Alianzas: ${response.connections["group-affiliation"]}</p>
               </div>
-              <div class="card-footer border-top-1 border-danger-subtle">
-              <em class="card-tex pt-4"><small> Publicado por: ${response.biography.publisher}</small></em>
+              <div class="card-footer border-top-1 border-danger-subtle d-flex align-items-center justify-content-between pt-5 px-4">
+              <p class="card-tex pt-4"><small> Publicado por: ${response.biography.publisher}</small></p>
+              <img src="/assets/img/sh2.jpg" class="img icono rounded-circle object-fit-cover" alt="...">
               </div>
           </div>
         </div>
@@ -69,14 +72,18 @@ jQuery.fn.buscaHero = function(token, idNumber) {
       }
     }
 
-    if (chartData.length === 0) { // Verifica si el array está vacío
+    if (chartData.length === 0) { // Verifica si el array está vacío y muestra mensaje sin estadísticas
       $("#chartContainer").html(`
-        <p class='text-light fs-3 text-center'>Epic fail! Sin estadísticas</p>
-        <img class='img-fluid w-50 text-center' src='assets/img/xxface.png' alt='cara x_x'>
+        <div class="row d-flex flex-column align-items-center">
+        <h3 class='text-danger fs-3 text-center'>Epic fail! Sin estadísticas</h3>
+        <i class="fa-solid fa-face-dizzy face-dead text-warning"></i>
+        </div>
       `);
     } else {
+      // Si el array tiene datos, muestra el gráfico de CanvasJS
       let chart = new CanvasJS.Chart("chartContainer", {
         theme: "dark1",
+        backgroundColor: "#212529",
         title: {
           animationEnabled: true,
           text: `Estadísticas de Poder para ${response.name}`,
@@ -98,6 +105,4 @@ jQuery.fn.buscaHero = function(token, idNumber) {
       chart.render();
     }
   }
-
-
 }
